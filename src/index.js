f2ad72df61d0a9973bad30228f4882bd42fddc95
d <derekodai@gmail.com>
2019-02-07 12:00:08 -0500

    add moving piece

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import {game} from '../lib/game';
 
 
 document.addEventListener("DOMContentLoaded", () => {
-   let canvas = document.getElementById("canvas")
-   let ctx = canvas.getContext("2d");
+   let arena = document.getElementById("canvas")
+   let ctx = arena.getContext("2d");
       ctx.fillStyle = "rgba(0, 0, 0,0.85)";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.fillRect(0, 0, arena.width, arena.height);
       
    const newGame = game();
 });
