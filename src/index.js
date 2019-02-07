3c7cd84526d0a9859ec0b7a07e5f65f6df4ef4f9
d <derekodai@gmail.com>
2019-02-07 08:52:30 -0500

    add piece

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import {game} from '../lib/game';  
 
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
    let canvas = document.getElementById("canvas")
    let ctx = canvas.getContext("2d");
-      ctx.fillStyle = 'black';
+      ctx.fillStyle = "rgba(0, 0, 0,0.85)";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
       
-   const newGame = new game();
-
-
+   const newGame = game();
 });
+
